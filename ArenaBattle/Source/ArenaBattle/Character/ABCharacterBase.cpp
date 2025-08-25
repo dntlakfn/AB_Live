6ae2d160b06bc799360df9c3a3dbdb6b8500feb8
dntlakfn <gun20010805@gmail.com>
2025-08-25 20:31:21 +0900

    create InputAction

--- ArenaBattle/Source/ArenaBattle/Character/ABCharacterBase.cpp
+++ ArenaBattle/Source/ArenaBattle/Character/ABCharacterBase.cpp
@@ -11,6 +11,12 @@ AABCharacterBase::AABCharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Pawn
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+
 	//Capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
 	GetCapsuleComponent()->SetCollisionProfileName(TEXT("Pawn"));
@@ -18,6 +24,8 @@ AABCharacterBase::AABCharacterBase()
 	//Movement
 	GetCharacterMovement()->JumpZVelocity = 700.0f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
 
 
 	//Mesh
--- ArenaBattle/Source/ArenaBattle/Character/ABCharacterPlayer.cpp
+++ ArenaBattle/Source/ArenaBattle/Character/ABCharacterPlayer.cpp
@@ -4,6 +4,8 @@
 #include "Character/ABCharacterPlayer.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 
 AABCharacterPlayer::AABCharacterPlayer()
@@ -11,15 +13,24 @@ AABCharacterPlayer::AABCharacterPlayer()
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(GetRootComponent());
 	SpringArm->TargetArmLength = 550.0f;
+	SpringArm->bUsePawnControlRotation = true;
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(SpringArm);
+	FollowCamera->bUsePawnControlRotation = false;
 }
 
 void AABCharacterPlayer::BeginPlay()
 {
 	Super::BeginPlay();
 
+	APlayerController* PlayerController = CastChecked<APlayerController>(GetController());
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+	if(Subsystem)
+	{
+		Subsystem->AddMappingContext(IMCDefault, 0);
+	}
 }
 
 void AABCharacterPlayer::Tick(float DeltaTime)
@@ -30,4 +41,34 @@ void AABCharacterPlayer::Tick(float DeltaTime)
 void AABCharacterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInputComponent->BindAction(IAJump, ETriggerEvent::Started, this, &ACharacter::Jump);
+	EnhancedInputComponent->BindAction(IAJump, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+	EnhancedInputComponent->BindAction(IALook, ETriggerEvent::Triggered, this, &AABCharacterPlayer::Look);
+	EnhancedInputComponent->BindAction(IAMove, ETriggerEvent::Triggered, this, &AABCharacterPlayer::Move);
+
+}
+
+void AABCharacterPlayer::Look(const FInputActionValue& Value)
+{
+	FVector2D LookVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookVector.X);
+	AddControllerPitchInput(LookVector.Y);
+}
+
+void AABCharacterPlayer::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	const FVector ForwardVertor = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardVertor, MovementVector.X);
+	AddMovementInput(RightVector, MovementVector.Y);
 }
